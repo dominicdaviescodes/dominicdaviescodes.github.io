7210eb15e34e8c4df92f751b16f99c13f98ccd51
dominicdaviescodes <codewithdom@gmail.com>
2020-03-30 21:29:29 +0100

    add sidebar navigation

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
+})(jQuery); // End of use strict
