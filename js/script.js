920a1ed514a5227a41509cb81145ddab7c05f035
dominicdaviescodes <codewithdom@gmail.com>
2020-03-31 22:34:48 +0100

    add scroll to top feature

--- js/script.js
+++ js/script.js
@@ -30,4 +30,14 @@
     }
   });
 
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
 })(jQuery); // End of use strict
